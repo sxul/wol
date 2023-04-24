597a12276ed436b02179c7a7addd1dcdbfe62177
sxul07 <sxul07@hotmail.com>
2023-04-25 06:59:26 +0800

    Optimized exception treatment

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ipnet::Ipv4Net;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
+use std::path::PathBuf;
 
 const WOL_PORT: u16 = 9;
 const MAGIC_HEADER: [u8; 6] = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
@@ -15,6 +16,7 @@ fn main() {
         .about("Wake up devices on the network")
         .arg(
             Arg::new("mac_address")
+                .value_name("MAC_ADDRESS")
                 .help("Target MAC address, e.g. 00:11:22:33:44:55")
                 .required_unless_present("file"), 
         )
@@ -23,6 +25,7 @@ fn main() {
                 .short('f')
                 .long("file")
                 .value_name("FILE")
+                .value_parser(clap::value_parser!(PathBuf))
                 .help("Reads target MAC addresses from a file, one per line. If this option is used, the mac_address option is ignored. Lines starting with # or // are ignored."),
         )
         .arg(
@@ -30,7 +33,7 @@ fn main() {
                 .short('n')
                 .long("net")
                 .value_name("NET")
-                .help("Specify the network segment to send the broadcast, e.g. 192.168.1.0/24"),
+                .help("Specify the network address to send the broadcast, use CIDR notation, e.g. 192.168.1.0/24"),
         )
         .arg(
             Arg::new("verbose")
@@ -43,7 +46,16 @@ fn main() {
 
     let verbose_mode = matches.get_flag("verbose");
 
-    let mac_addresses = if let Some(file_path) = matches.get_one::<String>("file") {
+    let mac_addresses = if let Some(file_path) = matches.get_one::<PathBuf>("file") {
+        // if file not exist or is not file, it will return with error code
+        if !file_path.exists() || !file_path.is_file() {
+            println!(
+                "Error: file not exist or is not file, input file path: {:?}",
+                file_path
+            );
+            std::process::exit(1);
+        }
+
         read_mac_addresses_from_file(file_path)
     } else {
         vec![matches
@@ -53,20 +65,47 @@ fn main() {
     };
 
     let networks = if let Some(custom_net) = matches.get_one::<String>("net") {
-        vec![custom_net.parse().unwrap()]
+        vec![match custom_net.parse() {
+            Ok(v) => v,
+            Err(err) => {
+                println!(
+                    "Error: {}, valied address with CIDR notation, e.g. 192.168.1.0/24",
+                    err
+                );
+                std::process::exit(1);
+            }
+        }]
     } else {
         get_local_ip_nets()
     };
 
     for mac_address in &mac_addresses {
-        send_wol_packet(mac_address, &networks, verbose_mode);
+        // check the mac address format
+        if mac_address.len() != 17 {
+            println!(
+                "Error: invalid MAC address length (should be 17), original MAC address: {}",
+                mac_address
+            );
+            continue;
+        }
+        if !mac_address.contains(':') {
+            println!(
+                "Error: invalid MAC address format (should be separated by :), original MAC address: {}",
+                mac_address
+            );
+            continue;
+        }
+
+        // uppercase the mac address
+        let mac_address = mac_address.to_uppercase();
+
+        send_wol_packet(&mac_address, &networks, verbose_mode);
     }
 }
 
 fn send_wol_packet(mac_address: &str, networks: &Vec<Ipv4Net>, verbose_mode: bool) {
-    let socket = UdpSocket::bind(
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0)
-    ).unwrap();
+    let socket =
+        UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0)).unwrap();
     for broadcast_net in networks {
         match send_magic_packet(&socket, mac_address, &broadcast_net) {
             Ok(_) => {}
@@ -91,16 +130,23 @@ fn send_magic_packet(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mac_parts: Vec<&str> = target_mac.split(':').collect();
     if mac_parts.len() != 6 {
-        return Err("Invalid MAC address format".into());
+        return Err("Invalid MAC address format, should be 6 parts".into());
     }
 
     let mut mac_bytes = [0u8; 6];
     for (i, part) in mac_parts.iter().enumerate() {
-        mac_bytes[i] = u8::from_str_radix(part, 16)?;
+        // parse the mac address
+        if part.len() != 2 {
+            return Err("Invalid MAC address format, should be 2 characters per part".into());
+        }
+        mac_bytes[i] = match u8::from_str_radix(part, 16) {
+            Ok(v) => v,
+            Err(_) => return Err("Invalid MAC address format, should be hex".into()),
+        }
     }
 
     let mut magic_packet = [0u8; 102];
-    
+
     magic_packet[..6].copy_from_slice(&MAGIC_HEADER);
 
     for i in 0..16 {
@@ -137,13 +183,20 @@ fn get_local_ip_nets() -> Vec<Ipv4Net> {
     ip_nets
 }
 
-fn read_mac_addresses_from_file(file_path: &str) -> Vec<String> {
+fn read_mac_addresses_from_file(file_path: &PathBuf) -> Vec<String> {
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
     let mut mac_addresses = Vec::new();
 
     for line in reader.lines() {
-        let line = line.unwrap().trim().to_string();
+        let line = match line {
+            Ok(v) => v.trim().to_string(),
+            Err(_) => break, // if read line error, break the loop
+        };
+        // skip empty lines
+        if line.is_empty() {
+            continue;
+        }
         // skip line starts with #
         if line.starts_with('#') {
             continue;
@@ -152,10 +205,15 @@ fn read_mac_addresses_from_file(file_path: &str) -> Vec<String> {
         if line.starts_with("//") {
             continue;
         }
-        // skip empty lines
-        if !line.is_empty() {
-            mac_addresses.push(line);
+        // check the mac address length (should be 17)
+        if line.len() != 17 {
+            continue;
         }
+        // invalid MAC address format (should be separated by :)
+        if !line.contains(':') {
+            continue;
+        }
+        mac_addresses.push(line);
     }
 
     mac_addresses
